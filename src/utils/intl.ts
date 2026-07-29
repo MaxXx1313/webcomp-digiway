40338b5a9ffb340cfec065299bc5c2fbc994f82d
Maksim Chertkov <maxch@unl.solutions>
2026-07-30 00:41:57 +0200

    #3 fix reuse

--- src/utils/intl.ts
+++ src/utils/intl.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 /**
  */
 export function formatNumber(num: number | string | undefined, language: string): string {
